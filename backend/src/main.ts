fca63e1be2e06d4e3053487e4520e0cc1681adf6
Bruno Nogueira <nogueirabruno459@gmail.com>
2026-07-23 15:19:46 -0300

    fix:correcao

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,9 +10,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200',
-    'https://dashboard-financeira-blush.vercel.app'],
-    credentials: true,
+    origin: [
+    'http://localhost:4200',
+    'https://dashboard-financeira-blush.vercel.app',
+    /\.vercel\.app$/ // Permite qualquer subdomínio da Vercel
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
